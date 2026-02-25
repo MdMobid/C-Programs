af6db9ca25549284bfb406da05f77965e76648d7
MdMobid <mobid9021@outlook.com>
2026-02-25 18:11:57 +0000

    LA1602 (Fix)

--- LAB16/LA1602.c
+++ LAB16/LA1602.c
@@ -12,7 +12,7 @@ void sort(int arr[], int len){
     int temp;
 
     for (int i=0; i<len-1; i++){
-        for (int j=0; j<len-i-1; j++){
+        for (int j=0; j<len-1; j++){
             if (arr[j] > arr[j+1]){
                 temp = arr[j];
                 arr[j] = arr[j+1];
