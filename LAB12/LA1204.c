b4dead09a92820fd70005ccc4dc1a7a6e47191eb
MdMobid <mobid9021@outlook.com>
2026-02-26 10:46:06 +0530

    LA1204 (Fix)

--- LAB12/LA1204.c
+++ LAB12/LA1204.c
@@ -1,19 +1,26 @@
 // WAP to reverse the array elements
-
 #include <stdio.h>
+
 int main(){
     int len;
     printf("Enter array size: ");
     scanf("%d", &len);
 
     int arr[len];
+
     printf("Enter the %d Numbers: ", len);
     for (int i = 0; i < len; i++){
         scanf("%d", &arr[i]);
     }
 
-    printf("Reverse Order:\n");
-    for (int i=len-1; i>=0; i--){
+    for (int i = 0; i < len/2; i++){
+        int temp = arr[i];
+        arr[i] = arr[len - 1 - i];
+        arr[len - 1 - i] = temp;
+    }
+
+    printf("Reversed Array:\n");
+    for (int i = 0; i < len; i++){
         printf("%d ", arr[i]);
     }
 
