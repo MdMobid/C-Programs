01d9b8f3b27678b3554f5b15cf8a2b4b0f82446b
MdMobid <mobid9021@outlook.com>
2026-03-14 10:26:19 +0530

    LA1201 (Fix)

--- LAB12/LA1201.c
+++ LAB12/LA1201.c
@@ -12,8 +12,8 @@ int main(){
         scanf("%d", &arr[i]);
     }
 
-    for (int i=0; i<len; i++){
-        for (int j=0; j<len; j++){
+    for (int i=0; i<len-1; i++){
+        for (int j=0; j<len-1; j++){
             if (arr[j] > arr[j+1]){
                 int temp = arr[j];
                 arr[j] = arr[j+1];
