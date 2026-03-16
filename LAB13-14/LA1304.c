496958fe0a8c5640f58060ef922f334e0defc290
MdMobid <mobid9021@outlook.com>
2026-03-16 10:26:01 +0530

    LA1304 (Fix)

--- LAB13-14/LA1304.c
+++ LAB13-14/LA1304.c
@@ -23,7 +23,7 @@ int main(){
    int sum = 0;
    for(int i=0; i<rows; i++){
       for (int j=0; j<cols; j++){
-        if (j < i) sum += arr[i][j];
+        if (j > i) sum += arr[i][j];
       }
     }
    printf("Sum of upper triangular matrix: %d", sum);   
