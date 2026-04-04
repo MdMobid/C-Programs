d7150dbcc4982f8e917d16f4ffc2d8ecb2b4d0f5
MdMobid <mobid9021@outlook.com>
2026-04-05 00:11:04 +0530

    LA1904 (New Method)

--- LAB19/LA1904.c
+++ LAB19/LA1904.c
@@ -9,14 +9,13 @@ int main(){
     fgets(str1, 100, stdin);
 
     int len = 0;
-    while (str1[len] != '\0') len++;
+    while (str1[len] != '\0' && str1[len] != '\n') len++;
 
-    int i = 0, j = len-2;
+    int i = 0;
     int flag = 0;
-    while (i < j/2){
-        if (str1[i] != str1[j]) flag = 1;
+    for(i=0; i<len; i++){
+        if (str1[i] != str1[len - i -1]) flag = 1;
         i++;
-        j--;
     }
 
     if (flag) printf("Not Palindrome");
