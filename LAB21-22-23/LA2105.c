d5486581d42c04b57b7b836bfc4d4eb025729463
MdMobid <mobid9021@outlook.com>
2026-04-05 00:48:10 +0530

    LA2105 (Fix)

--- LAB21-22-23/LA2105.c
+++ LAB21-22-23/LA2105.c
@@ -4,5 +4,23 @@
 
 int main(){
     char s[100];
-    
+    printf("Enter a string: ");
+    scanf("%s", s);
+
+    char* p = s;
+    int v=0, c=0, i=0;
+
+    while(*(p+i) != '\0'){
+        if ((*(p+i)>='a' && *(p+i)<='z') || (*(p+i)>='A' && *(p+i)<='Z')){
+            if (*(p+i) == 'a' || *(p+i) == 'e' || *(p+i) == 'i' || *(p+i) == 'o' || *(p+i) == 'u' ||
+                *(p+i) == 'A' || *(p+i) == 'E' || *(p+i) == 'I' || *(p+i) == 'O' || *(p+i) == 'U')
+            v++;
+            else c++;
+        }
+        i++;
+    }
+
+    printf("Vowels: %d\n", v);
+    printf("Consonants: %d\n", c);
+    return 0;
 }
